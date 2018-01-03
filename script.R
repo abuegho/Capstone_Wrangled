895033b36a503ee30f57ec315e51bc5d24abc389
abuegho <eghof@unlv.nevada.edu>
2018-01-03 01:43:58 -0800

    inter

--- script.R
+++ script.R
@@ -5,10 +5,17 @@ Traffic_W = rename(Traffic_Violations, Date = "Date Of Stop",
 
 ## Check for missing values and decide what you want to do about them
 
-Traffic_W = Traffic_Violations %>% 
-  filter(is.na(Traffic_W$Description) == F) %>% 
-  filter(is.na(Traffic_W$Make) == F) %>% 
-  filter(is.na(Traffic_W$`DL State`) == F)
+Traffic_W = filter(Traffic_W, is.na(Traffic_W$Description) == F)
+Traffic_W = filter(Traffic_W, is.na(Traffic_W$Make) == F)
+Traffic_W = filter(Traffic_W, Traffic_W$Make != "NONE")
+Traffic_W = filter(Traffic_W, is.na(Traffic_W$`DL State`) == F)
 
 # over 79,000 entries have Null Geolocations
-# about 10,000 violations involve a "Foot Patrol", which may indicate a non-automobile violation, but that is not the case. Many of those violations do involve vehicles
+
+## Make sure the values in each column make sense. If you find values that don't,
+## decide what you want to do about those. Look for outliers. Do they make sense? 
+## Do you want to keep them in the data set?
+
+Traffic_W = Traffic_W %>% 
+  filter(HAZMAT == "No") %>% 
+  select(-c(HAZMAT, Article, Agency))
